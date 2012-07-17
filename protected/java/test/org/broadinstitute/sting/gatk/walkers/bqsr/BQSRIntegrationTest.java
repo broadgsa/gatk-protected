f657b8bda8af29e24aa8b5a7c90aec11d3f0503f
Eric Banks <ebanks@broadinstitute.org>
2012-07-17 00:32:34 -0400

    Complete overhaul of the BQSRv2 integration tests.  Much more comprehensive.  Still need to deal with a few tests that need some modifications before I'm done, but I'll take care of that sometime tomorrow.

--- protected/java/test/org/broadinstitute/sting/gatk/walkers/bqsr/BQSRIntegrationTest.java
+++ protected/java/test/org/broadinstitute/sting/gatk/walkers/bqsr/BQSRIntegrationTest.java
@@ -1,35 +1,126 @@
 package org.broadinstitute.sting.gatk.walkers.bqsr;
 
 import org.broadinstitute.sting.WalkerTest;
+import org.broadinstitute.sting.utils.exceptions.UserException;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
 /**
- * @author carneiro
- * @since 3/27/12
+ * @author ebanks
+ * @since 7/16/12
  */
 public class BQSRIntegrationTest extends WalkerTest {
-    @Test(enabled = true)
-    public void recalibrateTest() {
-        String REF = "public/testdata/exampleFASTA.fasta";
-        String BAM = "public/testdata/exampleBAM.bam";
-        String DBSNP = "public/testdata/exampleDBSNP.vcf";
-        String base = String.format("-T BaseQualityScoreRecalibrator -R %s -I %s -knownSites %s", REF, BAM, DBSNP) + " -o %s ";
-        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("583517f4c0b480268edf3c36755e07a8"));
-        executeTest("recalibrateTest", spec);
+
+    private static class BQSRTest {
+        final String reference;
+        final String interval;
+        final String bam;
+        final String args;
+        final String md5;
+
+        private BQSRTest(String reference, String bam, String interval, String args, String md5) {
+            this.reference = reference;
+            this.bam = bam;
+            this.interval = interval;
+            this.args = args;
+            this.md5 = md5;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("BQSR(bam='%s', args='%s')", bam, args);
+        }
     }
 
-    @Test(enabled = true)
-    public void onTheFlyRecalibrationTest() {
-        String REF = "public/testdata/exampleFASTA.fasta";
-        String BAM = "public/testdata/exampleBAM.bam";
-        String GRP = "public/testdata/exampleGRP.grp";
-        String base = String.format("-T PrintReads -qq -1 -R %s -I %s -BQSR %s", REF, BAM, GRP) + " -o %s ";
-        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("690c7ab414bd39de72a635aedc1fcd66"));
-        executeTest("onTheFlyRecalibrationTest", spec);
+    @DataProvider(name = "BQSRTest")
+    public Object[][] createBQSRTestData() {
+        String HiSeqBam = privateTestDir + "HiSeq.1mb.1RG.bam";
+        String HiSeqInterval = "chr1:10,000,000-10,100,000";
+        return new Object[][]{
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, "", "93c63489bf274f68e0378a6c296b2948")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --no_standard_covs -cov ContextCovariate", "3bb9f93dc5a2eb879099402faf352a01")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --no_standard_covs -cov CycleCovariate", "c1321f6548bd240b174a8afe62bb07b9")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --indels_context_size 4", "d9512cebf54ea120539059976b33d1ca")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --low_quality_tail 5", "b29f83d42fc796b60ebe14a768e4f3af")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --quantizing_levels 6", "afa916b4d3a57d62b9b3e4f64b97f428")},
+                {new BQSRTest(hg18Reference, HiSeqBam, HiSeqInterval, " --mismatches_context_size 4", "5e305384496fb5ff64f7494092c939cf")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA12892.SLX.SRP000031.2009_06.selected.bam", "1:10,000,000-10,200,000", "", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA19240.chr1.BFAST.SOLID.bam", "1:10,000,000-10,200,000", "", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA12873.454.SRP000031.2009_06.chr1.10_20mb.bam", "1:10,000,000-10,200,000", "", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA12878.1kg.p2.chr1_10mb_11_mb.allTechs.bam", "1:10,800,000-10,810,000", "", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "originalQuals.1kg.chr1.1-1K.bam", "1:1-1,000", " -OQ", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA19240.chr1.BFAST.SOLID.bam", "1:10,000,000-20,000,000", " --solid_recal_mode REMOVE_REF_BIAS", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA12892.SLX.SRP000031.2009_06.selected.bam", "1:10,000,000-10,200,000", " -knownSites:anyNameABCD,VCF " + privateTestDir + "vcfexample3.vcf", "")},
+                //{new BQSRTest(b36KGReference, validationDataLocation + "NA12892.SLX.SRP000031.2009_06.selected.bam", "1:10,000,000-10,200,000", " -knownSites:bed " + validationDataLocation + "recalibrationTest.bed", "")},
+        };
     }
 
-    // TODO -- add test for -qq and -NIQ arguments
+    @Test(dataProvider = "BQSRTest")
+    public void testBQSR(BQSRTest params) {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                "-T BaseQualityScoreRecalibrator" +
+                        " -R " + params.reference +
+                        " -I " + params.bam +
+                        " -L " + params.interval +
+                        params.args +
+                        " --no_plots" +
+                        " -knownSites " + (params.reference.equals(b36KGReference) ? b36dbSNP129 : hg18dbSNP132) +
+                        " -o %s",
+                Arrays.asList(params.md5));
+        executeTest("testBQSR-"+params.args, spec).getFirst();
+    }
 
+    @Test
+    public void testBQSRFailWithoutDBSNP() {
+        WalkerTest.WalkerTestSpec spec = new WalkerTest.WalkerTestSpec(
+                "-R " + b36KGReference +
+                        " -T BaseQualityScoreRecalibrator" +
+                        " -I " + validationDataLocation + "NA12892.SLX.SRP000031.2009_06.selected.bam" +
+                        " -L 1:10,000,000-10,200,000" +
+                        " --no_plots" +
+                        " -o %s",
+                1, // just one output file
+                UserException.CommandLineException.class);
+        executeTest("testBQSRFailWithoutDBSNP", spec);
+    }
+
+    private static class PRTest {
+        final String args;
+        final String md5;
+
+        private PRTest(String args, String md5) {
+            this.args = args;
+            this.md5 = md5;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("PrintReads(args='%s')", args);
+        }
+    }
+
+    @DataProvider(name = "PRTest")
+    public Object[][] createPRTestData() {
+        return new Object[][]{
+                {new PRTest("", "d2d6ed8667cdba7e56f5db97d6262676")},
+                {new PRTest(" -qq -1", "b7053d3d67aba6d8892f0a60f0ded338")},
+                {new PRTest(" -qq 6", "bfbf0855185b2b70aa35237fb71e4487")},
+                {new PRTest(" -NIQ", "66aa65223f192ee39c1773aa187fd493")}
+        };
+    }
+
+    @Test(dataProvider = "PRTest")
+    public void testPR(PRTest params) {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                "-T PrintReads" +
+                        " -R " + hg18Reference +
+                        " -I " + privateTestDir + "HiSeq.1mb.1RG.bam" +
+                        " -BQSR " + privateTestDir + "HiSeq.1mb.1RG.table" +
+                        params.args +
+                        " -o %s",
+                Arrays.asList(params.md5));
+        executeTest("testPrintReads-"+params.args, spec).getFirst();
+    }
 }
