95a4ba57bf1d13ee48402231495c19a0eed35546
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-11-13 01:18:37 -0500

    Implementation of BySampleSAMFileWriter ReduceReads now works with the n-way-out capability, splitting by sample. DEV-27 #resolve #time 3m

--- protected/java/src/org/broadinstitute/sting/gatk/walkers/compression/reducereads/ReduceReads.java
+++ protected/java/src/org/broadinstitute/sting/gatk/walkers/compression/reducereads/ReduceReads.java
@@ -25,6 +25,9 @@
 
 package org.broadinstitute.sting.gatk.walkers.compression.reducereads;
 
+import net.sf.samtools.SAMFileHeader;
+import net.sf.samtools.SAMFileWriter;
+import net.sf.samtools.SAMProgramRecord;
 import net.sf.samtools.util.SequenceUtil;
 import org.broadinstitute.sting.commandline.Argument;
 import org.broadinstitute.sting.commandline.Hidden;
@@ -45,6 +48,7 @@ import org.broadinstitute.sting.utils.Utils;
 import org.broadinstitute.sting.utils.clipping.ReadClipper;
 import org.broadinstitute.sting.utils.exceptions.ReviewedStingException;
 import org.broadinstitute.sting.utils.help.DocumentedGATKFeature;
+import org.broadinstitute.sting.utils.sam.BySampleSAMFileWriter;
 import org.broadinstitute.sting.utils.sam.GATKSAMRecord;
 import org.broadinstitute.sting.utils.sam.ReadUtils;
 
@@ -86,7 +90,8 @@ import java.util.*;
 public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceReadsStash> {
 
     @Output
-    private StingSAMFileWriter out;
+    private StingSAMFileWriter out = null;
+    private SAMFileWriter writerToUse = null;
 
     /**
      * The number of bases to keep around mismatches (potential variation)
@@ -197,6 +202,10 @@ public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceRea
     private int nContigs = 2;
 
     @Hidden
+    @Argument(fullName = "nwayout", shortName = "nw", doc = "", required = false)
+    private boolean nwayout = false;
+
+    @Hidden
     @Argument(fullName = "", shortName = "dl", doc = "", required = false)
     private int debugLevel = 0;
 
@@ -227,6 +236,7 @@ public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceRea
     SortedSet<GenomeLoc> intervalList;
     
     private static final String PROGRAM_RECORD_NAME = "GATK ReduceReads";   // The name that will go in the @PG tag
+    private static final String PROGRAM_FILENAME_EXTENSION = ".reduced.bam";
 
     /**
      * Basic generic initialization of the readNameHash and the intervalList. Output initialization
@@ -242,10 +252,24 @@ public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceRea
         if (toolkit.getIntervals() != null)
             intervalList.addAll(toolkit.getIntervals());
 
-        if (!NO_PG_TAG)
-            Utils.setupWriter(out, toolkit, false, true, this, PROGRAM_RECORD_NAME);
-        else
+
+        // todo -- rework the whole NO_PG_TAG thing
+        final boolean preSorted = true;
+        final boolean indexOnTheFly = true;
+        final boolean generateMD5 = true;
+        final boolean keep_records = true;
+        final SAMFileHeader.SortOrder sortOrder = SAMFileHeader.SortOrder.coordinate;
+        if (nwayout) {
+            SAMProgramRecord programRecord = NO_PG_TAG ? null : Utils.createProgramRecord(toolkit, this, PROGRAM_RECORD_NAME);
+            writerToUse = new BySampleSAMFileWriter(toolkit, PROGRAM_FILENAME_EXTENSION, sortOrder, preSorted, indexOnTheFly, NO_PG_TAG, programRecord, true);
+        }
+        else {
+            writerToUse = out;
             out.setPresorted(false);
+            if (!NO_PG_TAG) {
+                Utils.setupWriter(out, toolkit, !preSorted, keep_records, this, PROGRAM_RECORD_NAME);
+            }
+        }
     }
 
     /**
@@ -386,6 +410,9 @@ public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceRea
         // output any remaining reads in the compressor
         for (GATKSAMRecord read : stash.close())
             outputRead(read);
+
+        if (nwayout)
+            writerToUse.close();
     }
 
     /**
@@ -554,7 +581,7 @@ public class ReduceReads extends ReadWalker<LinkedList<GATKSAMRecord>, ReduceRea
         if (!DONT_COMPRESS_READ_NAMES)
             compressReadName(read);
 
-        out.addAlignment(read);
+        writerToUse.addAlignment(read);
     }
 
     /**
