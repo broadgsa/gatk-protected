7df47418d839182cabd90839cea94da400d1d8eb
Mark DePristo <depristo@broadinstitute.org>
2013-01-03 14:43:36 -0500

    BQSR optimization: make RecalibrationTables thread-local, and merge results in onTraversalDone
    -- With the newer, faster BQSR, scaling was limited by the NestedIntegerArray.  The solution to this is to make the entire table thread-local, so that each nct thread has its own data and doesn't have any collisions.
    -- Removed the previous partial solution of having a thread-local quality score table
    -- Added a new argument -lowMemory

--- protected/java/src/org/broadinstitute/sting/gatk/walkers/bqsr/AdvancedRecalibrationEngine.java
+++ protected/java/src/org/broadinstitute/sting/gatk/walkers/bqsr/AdvancedRecalibrationEngine.java
@@ -25,35 +25,21 @@ package org.broadinstitute.sting.gatk.walkers.bqsr;
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import org.apache.log4j.Logger;
 import org.broadinstitute.sting.utils.classloader.ProtectedPackageSource;
 import org.broadinstitute.sting.utils.collections.NestedIntegerArray;
 import org.broadinstitute.sting.utils.recalibration.EventType;
 import org.broadinstitute.sting.utils.recalibration.ReadCovariates;
 import org.broadinstitute.sting.utils.recalibration.RecalDatum;
+import org.broadinstitute.sting.utils.recalibration.RecalibrationTables;
 import org.broadinstitute.sting.utils.sam.GATKSAMRecord;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class AdvancedRecalibrationEngine extends StandardRecalibrationEngine implements ProtectedPackageSource {
-    private final static Logger logger = Logger.getLogger(AdvancedRecalibrationEngine.class);
-
-    final List<NestedIntegerArray<RecalDatum>> allThreadLocalQualityScoreTables = new LinkedList<NestedIntegerArray<RecalDatum>>();
-    private ThreadLocal<NestedIntegerArray<RecalDatum>> threadLocalQualityScoreTables = new ThreadLocal<NestedIntegerArray<RecalDatum>>() {
-        @Override
-        protected synchronized NestedIntegerArray<RecalDatum> initialValue() {
-            final NestedIntegerArray<RecalDatum> table = recalibrationTables.makeQualityScoreTable();
-            allThreadLocalQualityScoreTables.add(table);
-            return table;
-        }
-    };
-
     @Override
     public void updateDataForRead( final ReadRecalibrationInfo recalInfo ) {
         final GATKSAMRecord read = recalInfo.getRead();
         final ReadCovariates readCovariates = recalInfo.getCovariatesValues();
-        final NestedIntegerArray<RecalDatum> qualityScoreTable = getThreadLocalQualityScoreTable();
+        final RecalibrationTables tables = getRecalibrationTables();
+        final NestedIntegerArray<RecalDatum> qualityScoreTable = tables.getQualityScoreTable();
 
         for( int offset = 0; offset < read.getReadBases().length; offset++ ) {
             if( ! recalInfo.skip(offset) ) {
@@ -70,30 +56,10 @@ public class AdvancedRecalibrationEngine extends StandardRecalibrationEngine imp
                         if (keys[i] < 0)
                             continue;
 
-                        incrementDatumOrPutIfNecessary(recalibrationTables.getTable(i), qual, isError, keys[0], keys[1], keys[i], eventIndex);
+                        incrementDatumOrPutIfNecessary(tables.getTable(i), qual, isError, keys[0], keys[1], keys[i], eventIndex);
                     }
                 }
             }
         }
-    }
-
-    /**
-     * Get a NestedIntegerArray for a QualityScore table specific to this thread
-     * @return a non-null NestedIntegerArray ready to be used to collect calibration info for the quality score covariate
-     */
-    private NestedIntegerArray<RecalDatum> getThreadLocalQualityScoreTable() {
-        return threadLocalQualityScoreTables.get();
-    }
-
-    @Override
-    public void finalizeData() {
-        // merge in all of the thread local tables
-        logger.info("Merging " + allThreadLocalQualityScoreTables.size() + " thread-local quality score tables");
-        for ( final NestedIntegerArray<RecalDatum> localTable : allThreadLocalQualityScoreTables ) {
-            recalibrationTables.combineQualityScoreTable(localTable);
-        }
-        allThreadLocalQualityScoreTables.clear(); // cleanup after ourselves
-
-        super.finalizeData();
     }
 }
