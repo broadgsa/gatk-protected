9e858270d77d2e770fec8f4b5d547b6e0e0a76eb
Eric Banks <ebanks@broadinstitute.org>
2014-01-19 02:33:11 -0500

    Moving this test up one level to where it actually belongs.

--- protected/java/test/org/broadinstitute/sting/gatk/walkers/WalkerTestIntegrationTest.java
+++ protected/java/test/org/broadinstitute/sting/gatk/walkers/WalkerTestIntegrationTest.java
@@ -44,7 +44,7 @@
 *  7.7 Governing Law. This Agreement shall be construed, governed, interpreted and applied in accordance with the internal laws of the Commonwealth of Massachusetts, U.S.A., without regard to conflict of laws principles.
 */
 
-package org.broadinstitute.sting.gatk.walkers.annotator;
+package org.broadinstitute.sting.gatk.walkers;
 
 import org.broadinstitute.sting.WalkerTest;
 import org.testng.annotations.Test;
